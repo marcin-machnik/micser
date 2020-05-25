fa80d78dd2865a91645e1579b789d8795b15a66e
macmasazur <marcin.machnik.prv@gmail.com>
2020-05-25 12:36:20 +0200

    Working in cluster

--- microservice-monitor/src/main/java/com/rogaszy/micser/microservice/monitor/MonitorVerticle.java
+++ microservice-monitor/src/main/java/com/rogaszy/micser/microservice/monitor/MonitorVerticle.java
@@ -6,16 +6,28 @@ import com.rogaszy.micser.microservice.monitor.api.RestMonitorVerticle;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class MonitorVerticle extends BaseMicroserviceVerticle{
 
+	private static final Logger logger = LoggerFactory.getLogger(MonitorVerticle.class);
+	
 	@Override
 	public void start(Future<Void> startFuture) throws Exception {
 		super.start(startFuture);
 		
 		LossService service = LossService.createProxy(vertx, LossService.SERVICE_ADDRESS);
 		
-		deployRestService(service);
+//		deployRestService(service);
+		
+		service.retrieveLoss("small", r -> {
+			if (r.succeeded()) {
+				logger.info("small loss: " + r.result().toString() );
+			} else {
+				logger.info("FAILED: " + r.cause() );
+			}
+		});
 	}
 	
 	private Future<Void> deployRestService(LossService service) {
